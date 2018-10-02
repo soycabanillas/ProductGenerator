4192f575cefab85239a7927a8853a99fcf4ee825
Alejandro Cabanillas <admin@marketpay.io>
2018-10-02 12:06:20 +0200

    Paket working

--- build.fsx
+++ build.fsx
@@ -1,3 +1,24 @@
 #r "paket:
-nuget Fake.Core.Target //"
+nuget Fake.Core.Target
+nuget Fake.DotNet //"
 #load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.DotNet
+open Fake.Core
+open System.Diagnostics
+
+let processInstallPaket = new Process()
+processInstallPaket.StartInfo.FileName <- "dotnet"
+processInstallPaket.StartInfo.Arguments <- """ tool install paket --version "[5.179.403]" --tool-path ".paket" --add-source https://www.myget.org/F/paket-netcore-as-tool/api/v3/index.json"""
+//processInstallPaket.StartInfo.CreateNoWindow <- true;
+//processInstallPaket.StartInfo.UseShellExecute <- false;
+processInstallPaket.Start()
+printfn
+
+// File system information
+let solutionFile  = "ProductGenerator.sln"
+
+Target.create "Restore" (fun _ ->
+    solutionFile
+    |> DotNet.restore id
+)
